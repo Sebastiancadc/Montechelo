85bc659e6fdec1ac32f2f96a3318ba3f0d6e51d5
Sebastiancadc1 <sr928291@gmail.com>
2021-02-11 15:40:30 -0500

    evitar back,logout

--- resources/views/admin/layouts/layout.blade.php
+++ resources/views/admin/layouts/layout.blade.php
@@ -12,6 +12,13 @@ $connt = Illuminate\Support\Facades\DB::table('messages')->select('seen')->where
 $ids = Auth::user()->id;
 $conntss = Illuminate\Support\Facades\DB::select("SELECT seen FROM messages WHERE to_id = '$ids' AND seen = 0");
 $horas = Illuminate\Support\Facades\DB::select("SELECT MAX(created_at) as created_at FROM messages WHERE to_id = '$ids'");
+
+header("Expires: Tue, 01 Jul 2001 06:00:00 GMT");
+header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+header("Cache-Control: no-store, no-cache, must-revalidate");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+
 ?>
 {{-- <script>
   window.history.forward();
